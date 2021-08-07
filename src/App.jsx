82e8905df7b239e08df35d5cc286d56eccd67b4a
Jose <Jose.orbezo@alumnos.frgp.utn.edu.ar>
2021-08-07 19:48:49 -0300

    organizacion en components y pages, e implementacion de rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 //import logo from './logo.svg';
 import './App.css';
-import { MoviesGrid } from './MoviesGrid.jsx';
+import { MoviesGrid } from './components/MoviesGrid.jsx';
 import styles from "./App.module.css";
+import {BrowserRouter as Router,Switch,Route,Link} from "react-router-dom";
+import { LandingPage } from './pages/LandingPage';
+import { MovieDetails } from './pages/MovieDetails';
 //commit de prueba
 /*
   API SUPER HEROES
@@ -12,10 +15,19 @@ import styles from "./App.module.css";
 */
 function App() {
   return (
-    <div>
-      <header><h1 className={styles.title}>Movies</h1></header>        
-      <main><MoviesGrid/></main>        
-    </div>
+    <Router>
+      <header>
+        <Link to="/">
+        <h1 className={styles.title}>Movies</h1>
+        </Link>
+      </header>        
+      <main>
+        <Switch>
+          <Route exact path="/movies/:movieId"><MovieDetails/></Route>
+          <Route path="/"><LandingPage/></Route>        
+        </Switch>
+      </main>        
+    </Router>
   );
 }
 
